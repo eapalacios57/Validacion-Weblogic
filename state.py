7fc5e9d36478a1eab06b4cdb639174f95b5846dc
eapalacios57 <epalacios@qvision.us>
2022-12-14 15:18:40 -0500

    Ajustes Jenkisnfile

--- state.py
+++ state.py
@@ -1,5 +1,5 @@
 #Conexión a Consola Weblogic
-connect("devops","devops21","t3://10.1.5.153:3041")
+# connect("devops","devops21","t3://10.1.5.153:3041")
 
 #Validar Estado del Servidor (Cluster)
 # retMap=state ("Cluster_Transversales2", returnMap="true")
