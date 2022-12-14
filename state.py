fa21f4236a698767e36380abf48c7cf361ca8952
epalacios@qvision.us-Desarrollador <48025620+eapalacios57@users.noreply.github.com>
2022-12-14 15:26:13 -0500

    Update state.py

--- state.py
+++ state.py
@@ -1,6 +1,6 @@
 #Conexión a Consola Weblogic
 connect("devops","devops21","t3://10.1.5.153:3041")
 
-#Validar Estado del Servidor (Cluster)
+#Validar Estado del Servidor Weblogic(Cluster)
 retMap=state ("Cluster_Transversales2", returnMap="true")
 
